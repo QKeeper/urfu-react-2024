4952a7938bd2652ba1f1c609a85d00001548c7c0
QKeeper <moncer256710@gmail.com>
2024-09-13 20:46:51 +0500

    Fix API

--- src/api.ts
+++ src/api.ts
@@ -3,20 +3,22 @@ import { IPost } from "./models";
 
 const baseURL = import.meta.env.VITE_API;
 
-export const Post = {
+const Post = {
   instance: axios.create({ baseURL: baseURL + "/posts" }),
   get: async function (id?: string) {
-    return await this.instance.get("/post" + (id ? "?id=" + id : ""));
+    return await this.instance.get("" + (id ? "?id=" + id : ""));
   },
   create: async function (data: IPost) {
-    return await this.instance.post("/post", { data });
+    return await this.instance.post("", { data });
   },
   patch: async function (
     data: Omit<IPost, "id"> & Required<Pick<IPost, "id">>
   ) {
-    return await this.instance.patch("/post?id=" + data.id, { data });
+    return await this.instance.patch("?id=" + data.id, { data });
   },
   delete: async function (id?: string) {
-    return await this.instance.delete("/post?id=" + id);
+    return await this.instance.delete("?id=" + id);
   },
 };
+
+export const API = { Post };
