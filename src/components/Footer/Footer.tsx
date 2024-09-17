081be7d595fba0439a8692367bfe1e53d430bd80
QKeeper <moncer256710@gmail.com>
2024-09-17 22:21:29 +0500

    Changed footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,16 +2,16 @@ import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 function Footer() {
   return (
-    <footer className="flex bg-white text-gray-400">
+    <footer className="flex select-none bg-white text-gray-400">
       <div
         about="footer-container"
-        className="container flex h-10 items-center gap-4 text-sm"
+        className="container flex h-10 items-center justify-center gap-4 text-sm"
       >
         <p>&copy; Nikita Leshchenko 2024</p>
         <a
           target="_blank"
           href="https://github.com/QKeeper/urfu-react-2024"
-          className="flex items-center gap-1 underline"
+          className="relative flex items-center gap-1 hover:underline"
         >
           <GitHubLogoIcon />
           GitHub
